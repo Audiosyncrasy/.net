9d094b61ec5903db898e415ade13618583928c82
Audiosyncrasy <jerm.killer2002@gmail.com>
2021-08-05 07:55:29 -0700

    add GetArtistDetails stored proc script to sln

---
+++ DB/Stored Procedures/GetArtistDetails.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[GetArtistDetails]
+	@artistID INT
+AS
+BEGIN
+
+	SELECT *
+	FROM dbo.Artist
+	WHERE artistID = @artistID;
+
+	SELECT a.title AS artistName, s.title
+	FROM dbo.Song s
+	LEFT JOIN dbo.Artist a ON s.artistID = a.artistID
+	WHERE a.artistID = @artistID;
+
+	SELECT *
+	FROM dbo.Album
+	WHERE artistID = @artistID;
+
+END
+GO
