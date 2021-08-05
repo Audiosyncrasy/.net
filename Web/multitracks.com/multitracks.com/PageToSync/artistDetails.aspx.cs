3ad26344217775a2f4e6a9a30c50591cc60bb582
Audiosyncrasy <jerm.killer2002@gmail.com>
2021-08-05 07:35:09 -0700

    update db connection str and add artistDetails pg

---
+++ Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess;
+using System;
+
+public partial class ArtistDetails : MultitracksPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var sql = new SQL();
+
+        sql.Parameters.Add("@artistID", 5);
+        var data = sql.ExecuteStoredProcedureDS("GetArtistDetails");
+
+        artistDetails.DataSource = data;
+        artistDetails.DataBind();
+
+    }
+}
