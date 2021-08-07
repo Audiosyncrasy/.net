258d35ed70651a8740364d759394eeca1932c003
Audiosyncrasy <jerm.killer2002@gmail.com>
2021-08-07 12:16:16 -0700

    update artistDetails page to be data driven

--- Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
+++ Web/multitracks.com/multitracks.com/PageToSync/artistDetails.aspx.cs
@@ -5,13 +5,23 @@ public partial class ArtistDetails : MultitracksPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string[] tableNames = new string[] { "artistDetails", "songs", "albums" };
+
         var sql = new SQL();
 
-        sql.Parameters.Add("@artistID", 5);
-        var data = sql.ExecuteStoredProcedureDS("GetArtistDetails");
+        sql.Parameters.Add("@artistID", 107);
+        var data = sql.ExecuteStoredProcedureDS("GetArtistDetails").SetTableNames(tableNames);
 
-        artistDetails.DataSource = data;
+        artistDetails.DataSource = data.Tables["artistDetails"];
         artistDetails.DataBind();
 
+        topSongs.DataSource = data.Tables["songs"];
+        topSongs.DataBind();
+
+        albumList.DataSource = data.Tables["albums"];
+        albumList.DataBind();
+
+        artistBio.DataSource = data.Tables["artistDetails"];
+        artistBio.DataBind();
     }
 }
