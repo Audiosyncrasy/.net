9a3cac6fb47cc10af0d773cee5817447f7f95e85
Audiosyncrasy <jerm.killer2002@gmail.com>
2021-08-08 18:44:39 -0700

    add AddArtist stored procedure to solution

---
+++ DB/Stored Procedures/AddArtist.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE dbo.AddArtist
+	@title varchar(100),
+	@biography varchar(max),
+	@imageURL varchar(500),
+	@heroURL varchar(500)
+AS
+BEGIN
+
+	INSERT INTO dbo.Artist (
+		title,
+		biography,
+		imageURL,
+		heroURL
+	)
+	VALUES (
+		@title,
+		@biography,
+		@imageURL,
+		@heroURL
+	)
+	SELECT CAST(SCOPE_IDENTITY() AS int) AS artistID;
+
+END
